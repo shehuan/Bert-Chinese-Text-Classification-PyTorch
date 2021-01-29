f07938545b7d8c57423c48c943cbe6627f8d4577
shehuan <shehuan@nowledgedata.com.cn>
2021-01-29 16:27:25 +0800

    修改单条文本预测代码

--- models/BruceBert.py
+++ models/BruceBert.py
@@ -41,26 +41,6 @@ class Config(object):
         # bert 隐藏层个数
         self.hidden_size = 768
 
-    def build_predict_text(self, text):
-        token = self.tokenizer.tokenize(text)
-        token = ['[CLS]'] + token
-        seq_len = len(token)
-        mask = []
-        token_ids = self.tokenizer.convert_tokens_to_ids(token)
-        pad_size = self.pad_size
-        if pad_size:
-            if len(token) < pad_size:
-                mask = [1] * len(token_ids) + ([0] * (pad_size - len(token)))
-                token_ids += ([0] * (pad_size - len(token)))
-            else:
-                mask = [1] * pad_size
-                token_ids = token_ids[:pad_size]
-                seq_len = pad_size
-        ids = torch.LongTensor([token_ids])
-        seq_len = torch.LongTensor([seq_len])
-        mask = torch.LongTensor([mask])
-        return ids, seq_len, mask
-
 
 class Model(nn.Module):
     def __init__(self, config):
